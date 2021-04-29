06a95ea7f1d8652aa3eca4c2e8a5e628b84c095f
Mark Perkins <markeperk@gmail.com>
2021-04-29 11:39:31 -0700

    update pre

--- src/components/Views/Guide/Guide.jsx
+++ src/components/Views/Guide/Guide.jsx
@@ -60,10 +60,10 @@ const Guide = ({ guides }) => {
           The code
         </Typography>
         <Divider style={{ marginTop: 10, marginBottom: 10 }} />
-        <Typography variant='p' style={{ paddingTop: 10, paddingBottom: 10 }}>
+        <Typography variant='p' style={{ paddingTop: 10, paddingBottom: 12 }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </Typography>
-        <pre style={{ background: 'lightgray', fontFamily: 'sans-serif'}}>
+        <pre style={{ background: 'lightgray', fontFamily: 'sans-serif', fontSize: 12, color: 'gray' }}>
           {`        
             const tile = {
               type: "tile",
